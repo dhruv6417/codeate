7f504d5a4efca448952c06273d1b3d19449a1c00
Beasr1 <d.ishaan001@gmail.com>
2022-09-05 09:26:36 +0530

    day1_homePage_extras

---
+++ components/home/herosection/HeroSection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+// import { Button } from '../../Button';
+import Link from 'next/link';
+import styles from '../../../styles/home/HeroSection.module.css'
+
+function HeroSection({
+  topLine,
+  lightText,
+  lightTextDesc,
+  headline,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  imgStart
+}) {
+    
+  return (
+    <>
+      <div className={`${styles.home__hero_section}`}>
+        <div className={`${styles.container}`}>
+          <div className={`${styles.row} ${styles.home__hero_row}`}>
+            <div className={`${styles.col}`}>
+              <div className={`${styles.home__hero_text_wrapper}`}>
+                {/* <div className={`${styles.top_line}`}>{topLine}</div> */}
+                <h1 className={`${styles.heading}`}>
+                  {headline}
+                </h1>
+                <p className={`${styles.home__hero_subtitle}`} >
+                  {description}
+                </p>
+                {/* <Link to='/community'>
+                  <Button buttonSize='btn--large' buttonColor='blue'>
+                    {buttonLabel}
+                  </Button>
+                </Link> */}
+              </div>
+            </div>
+            <div className={`${styles.col}`}>
+              <div className={`${styles.home__hero_img_wrapper}`}>
+                <img src={img} alt={alt} className={`${styles.home__hero_img}`} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
+import HeroSection from '../components/home/herosection/HeroSection'
+import { Ouroffering } from '../components/home/ouroffering/Ouroffering'
+import { homeObjOne } from '../data/HomeData'
+import Aboutcode8 from '../components/home/aboutcode8/Aboutcode8'
 
 export default function Home() {
   return (
-   <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <HeroSection {...homeObjOne} />
+      <Ouroffering Heading={'Explore our programs'} />
+      <Aboutcode8/>
+      {/*<Stats />
+      <Ourreach />
+      <Testimonial />
+      <Eventgallery />
+      <OurPartners/>
+      <Ourcommunity /> */}
+    </>
   )
 }
